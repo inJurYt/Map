a7fd67bca581569a4935a8f5c19cb732ad414d64
linyuxu-black <591721409@qq.com>
2017-09-16 20:40:09 +0800

    基础地图显示，定位

--- app/src/main/java/com/example/magi/map/MainActivity.java
+++ app/src/main/java/com/example/magi/map/MainActivity.java
@@ -3,49 +3,103 @@ package com.example.magi.map;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
-    private MapView bmapView = null;
+    public BDAbstractLocationListener myListener = new MyLocationListener();
+    private MapView bmapView;
     private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        initView();
+        initMapView();
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener( myListener );
+        mLocationClient.setLocOption(getMapIption());
+        mLocationClient.start();
     }
 
-    private void initView() {
+    private void initMapView() {
         bmapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = bmapView.getMap();
         //普通地图
+        mBaiduMap.setMyLocationEnabled(true);
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
         //开启交通图
-        mBaiduMap.setTrafficEnabled(true);
+        //mBaiduMap.setTrafficEnabled(true);
         //卫星地图
         //mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+    }
 
+    private LocationClientOption getMapIption(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setWifiCacheTimeOut(30000);
+        return option;
+    }
+
+    @Override
+    protected void onStart()
+    {
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+        {
+            mLocationClient.start();
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+        super.onStop();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         bmapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         bmapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         bmapView.onPause();
+    }
+
+    private class MyLocationListener extends BDAbstractLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            MyLocationData locData = new MyLocationData.Builder().accuracy(location.getRadius()).latitude(location.getLatitude()).longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLngZoom(latLng, 18.0f);
+            mBaiduMap.animateMapStatus(mapStatusUpdate);
+        }
     }
 }
