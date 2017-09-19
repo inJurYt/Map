12175ea063ec2f8c3e1417965f77369b6d9a1488
linyuxu-black <591721409@qq.com>
2017-09-19 16:41:01 +0800

    添加搜索：首页、尾页（尾页由于百度API的问题无法使用）

--- app/src/main/java/com/example/magi/map/SearchActivity.java
+++ app/src/main/java/com/example/magi/map/SearchActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Xml;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
